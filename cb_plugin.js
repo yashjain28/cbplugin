b93d9f1a9084c452a423460e1e82ffe39a28feaa
Yash Jain <yashkjain1994@gmail.com>
2018-06-13 16:40:26 -0500

    Added data source

--- cb_plugin.js
+++ cb_plugin.js
@@ -127,3 +127,125 @@ function PluginTest(settings, updateCallback) {
         console.log("disposed");
     }
 }
+
+CB_PORTAL.registerDatasource({
+  // this function must be called in order to register the datasource as a plugin
+  type_name: "CB_PLUGIN_DATASOURCE",
+  display_name: "Datasource Plugin",
+  group: "ClearBlade Plugins",
+  description: "A datasource as a plugin.",
+  class: PluginDatasourceTest,
+  settings: [
+    {
+      name: "query",
+      display_name: "Search Query",
+      type: "text",
+      required: true
+    },
+    {
+      name: "apiKey",
+      display_name: "Google Search API Key",
+      type: "text",
+      required: true
+    },
+    {
+      name: "searchEngineId",
+      display_name: "Search Engine ID",
+      type: "text",
+      required: true
+    },
+    {
+      name: "dessert",
+      display_name: "Favorite Dessert?",
+      type: "text",
+      required: true
+    },
+    {
+      name: "booleanVal",
+      display_name: "Boolean Value",
+      type: "boolean",
+      default_value: true
+    },
+    {
+      name: "numberVal",
+      display_name: "Number Value",
+      type: "number",
+      default_value: 42
+    },
+    {
+      name: "colorVal",
+      display_name: "Color Value",
+      type: "color",
+      default_value: "#fff"
+    },
+    {
+      name: "optionVal",
+      display_name: "Option Value",
+      type: "option",
+      default_value: "option2",
+      options: [
+        {
+          name: "Option 1",
+          value: "option1"
+        },
+        {
+          name: "Option 2",
+          value: "option2"
+        }
+      ]
+    }
+  ]
+});
+
+
+function PluginDatasourceTest(settings, updateCallback, errorCallback) {
+  let payload = {
+    query: settings.query,
+    apiKey: settings.apiKey,
+    searchEngineId: settings.searchEngineId
+  };
+
+  this.onSettingsChanged = function(settings) {
+    payload = {
+      query: settings.query,
+      apiKey: settings.apiKey,
+      searchEngineId: settings.searchEngineId
+    };
+    this.updateNow();
+  };
+
+  this.updateNow = function() {
+    this.sendData(payload);
+  };
+
+  function handleErrors(response) {
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+    return response;
+  }
+
+  this.sendData = function(data) {
+    fetch(
+      `https://www.googleapis.com/customsearch/v1?q=${data.query}&key=${
+        data.apiKey
+      }&cx=${data.searchEngineId}`,
+      {
+        method: "GET"
+      }
+    )
+      .then(handleErrors)
+      .then(resp =>
+        resp.json().then(jsonResp => {
+          updateCallback(jsonResp);
+        })
+      )
+      .catch(e => {
+        errorCallback("Failed to search", e);
+      });
+  };
+
+  this.onDispose = function() {};
+
+  this.updateNow();
+}
